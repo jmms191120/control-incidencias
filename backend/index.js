af75b0bc427d2a278f467121e8685e7c874d16a4
Jose Moises Moya Sanchez <josemoises5ms@gmail.com>
2025-12-15 02:57:40 -0500

    fix cors configuration

--- backend/index.js
+++ backend/index.js
@@ -9,18 +9,22 @@ import usuariosRoutes from "./routes/usuariosRoutes.js";
 
 const app = express();
 
+// ✅ Middlewares
 app.use(cors());
 app.use(express.json());
 
+// ✅ Rutas
 app.use("/api/auth", authRoutes);
 app.use("/api/incidencias", incidenciasRoutes);
 app.use("/api/equipos", equiposRoutes);
 app.use("/api/usuarios", usuariosRoutes);
 
+// Ruta test
 app.get("/", (req, res) => {
   res.send("API Control Incidencias OK");
 });
 
+// Server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
